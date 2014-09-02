98d312f3301d690960d2cd76776e17c1ef142e30
Maxim Sergeev <sergeev@a2design.biz>
2014-09-02 11:41:34 +0700

    Updated: log page template

--- templates/a2-import-log.php
+++ templates/a2-import-log.php
@@ -1,5 +1,4 @@
-<h2>Import log</h2>
-
+<h3>Import log</h3>
 <div>
 	<textarea cols="80" rows="30" name="newcontent" id="newcontent" aria-describedby="newcontent-description"><?php $upload_dir = wp_upload_dir(); ?><?php echo (file_get_contents($upload_dir['baseurl'] . '/import-log/log', true)); ?>
 	</textarea>
