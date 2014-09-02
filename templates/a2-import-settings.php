72b679e173306da61bb66eecae038c2a47c6295a
Maxim Sergeev <sergeev@a2design.biz>
2014-09-02 13:41:07 +0700

    Added: plugin settings form

--- templates/a2-import-settings.php
+++ templates/a2-import-settings.php
@@ -1,1 +1,67 @@
-<h3>Import settings</h3>
+<form method="post" action="admin.php" novalidate="novalidate">
+	<h3>Database Settings</h3>
+	<hr>
+	<table class="form-table">
+		<tbody>
+			<tr>
+				<th scope="row"><label for="db-host">DB Host</label></th>
+				<td><input name="db[host]" type="text" id="db-host" value="" placeholder="127.0.0.1" class="regular-text"></td>
+			</tr>
+			<tr>
+				<th scope="row"><label for="db-user">DB User</label></th>
+				<td><input name="db[user]" type="text" id="db-user" placeholder="wp_user" value="" class="regular-text">
+			</tr>
+			<tr>
+				<th scope="row"><label for="db-pass">DB password</label></th>
+				<td><input name="db[pass]" type="password" id="db-pass" value="" placeholder="123" class="regular-text code"></td>
+			</tr>
+			<tr>
+				<th scope="row"><label for="db-name">DB Name</label></th>
+				<td><input name="db[name]" type="text" id="db-name" value="" placeholder="wp-my-site" class="regular-text code">
+			</tr>
+		</tbody>
+	</table>
+
+	<br>
+	<h3>Date Settings</h3>
+	<hr>
+
+	<table class="form-table">
+		<tbody>
+			<tr>
+				<th scope="row">Select time period</th>
+				<td>
+					<fieldset name="date"><legend class="screen-reader-text"><span>Date Format</span></legend>
+						<label title="">
+							<input type="radio" name="date_format" value="overall" checked="checked">
+							<span>Overall</span>
+						</label><br>
+						<label title="">
+							<input type="radio" name="date_format" value="select-period">
+							<span>From <input id="date-from" name="date[from]" type="date"> To <input id="date-to" name="date[to]" type="date"></span>
+						</label><br>
+					</fieldset>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+
+	<br>
+	<h3>Cron Settings</h3>
+	<hr>
+	<table class="form-table">
+		<tbody>
+		<tr>
+			<th scope="row">Enable cron</th>
+			<td>
+				<fieldset name="cron">
+					<label title=""><input type="radio" name="cron_period" value="overall" checked="checked"> <span>No</span></label><br>
+					<label title="Y-m-d"><input type="radio" name="cron_period" value="select period"> <span>Import posts every <input id="cron-period" name="cron-period[every_n_hours]" type="number"> hours</span></label><br>
+				</fieldset>
+			</td>
+		</tr>
+		</tbody>
+	</table>
+
+	<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Apply settings"></p>
+</form>
